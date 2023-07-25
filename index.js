bb7bc02ade05a14144a781b3f26b736d13b8952c
ujjwal1309 <ujjwalsharma1309@gmail.com>
2023-07-26 00:05:30 +0530

    done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.getElementById("resume-link-2").onclick = ()=>{
-    window.open("https://drive.google.com/file/d/188lGyqT4ZZ3LTYQ9EIKTVzoZKuqeabjy/view")
+    window.open("https://drive.google.com/file/d/1A5WpN0KvDctALvyCOO2gdNfTPN16SywW/view")
 }
 document.getElementById("resume-link-1").onclick = ()=>{
-    window.open("https://drive.google.com/file/d/188lGyqT4ZZ3LTYQ9EIKTVzoZKuqeabjy/view")
+    window.open("https://drive.google.com/file/d/1A5WpN0KvDctALvyCOO2gdNfTPN16SywW/view")
 }
 document.getElementById("meetease").onclick = ()=>{
     window.open("https://easemeet.onrender.com/")
@@ -11,19 +11,19 @@ document.getElementById("meetease").onmouseover = ()=>{
     document.getElementById("meetease").style.cursor = "pointer"
 }
 document.getElementById("beautyHeaven").onclick = ()=>{
-    window.open("https://beauty-heaven.onrender.com/")
+    window.open("https://649036113722945443c6387d--spiffy-unicorn-2d615b.netlify.app/index.html")
 }
 document.getElementById("beautyHeaven").onmouseover = ()=>{
     document.getElementById("beautyHeaven").style.cursor = "pointer"
 }
 document.getElementById("upwear").onclick = ()=>{
-    window.open("https://extraordinary-pavlova-536bcb.netlify.app/")
+    window.open("https://magenta-lion-hem.cyclic.app/")
 }
 document.getElementById("upwear").onmouseover = ()=>{
     document.getElementById("upwear").style.cursor = "pointer"
 }
 document.getElementById("groceryarc").onclick = ()=>{
-    window.open("https://lucent-valkyrie-3c86fc.netlify.app/")
+    window.open("https://singular-babka-0ed0b3.netlify.app/")
 }
 document.getElementById("groceryarc").onmouseover = ()=>{
     document.getElementById("groceryarc").style.cursor = "pointer"
@@ -31,12 +31,7 @@ document.getElementById("groceryarc").onmouseover = ()=>{
 // document.getElementById("anthropologie").onclick = ()=>{
 //     window.open("https://spontaneous-dolphin-60e6a7.netlify.app")
 // }
-document.getElementById("kindmeal").onclick = ()=>{
-    window.open("https://beamish-salamander-14c16f.netlify.app/")
-}
-document.getElementById("kindmeal").onmouseover = ()=>{
-    document.getElementById("kindmeal").style.cursor = "pointer"
-}
+
 
 const navbarToggle = document.getElementById('navbar-toggle');
 const navbarMenu = document.getElementById('navbar-menu');
